d681697cb043a0c5ee727c5797df182c272bff8c
catherinemathews <67663005+catherinemathews@users.noreply.github.com>
2022-12-10 18:19:43 -0500

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -20,15 +20,6 @@ with open(test_input, 'r') as ts_in:
     ts_content = ts_in.readlines()
 
 class Node:
-    """
-    Here is an arbitrary Node class that will form the basis of your decision
-    tree. 
-    Note:
-        - the attributes provided are not exhaustive: you may add and remove
-        attributes as needed, and you may allow the Node to take in initial
-        arguments as well
-        - you may add any methods to the Node class if desired 
-    """
     def __init__(self):
         self.left = None
         self.right = None
